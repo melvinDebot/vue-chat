41e2def6c77de69a4de3412fe5b7ef073bb44f1c
melvinDebot <melvindebot@gmail.com>
2020-06-07 16:25:34 +0200

    mise en prod

--- src/components/CreateMessage.vue
+++ src/components/CreateMessage.vue
@@ -2,10 +2,10 @@
     <div class="container" style="margin-bottom: 30px">
         <form @submit.prevent="createMessage">
             <div class="form-group">
-                <input type="text" name="message" class="form-control" placeholder="Enter message ..." v-model="newMessage">
+                <input type="text" name="message" class="form-control" placeholder="Ecrire un message ..." v-model="newMessage">
                 <p class="text-danger" v-if="errorText">{{ errorText }}</p>
             </div>
-            <button class="btn btn-primary" type="submit" name="action">Submit</button>
+            <button class="btn btn-primary" type="submit" name="action">Envoyer</button>
         </form>
     </div>
 </template>
